a819fd3c97a13b44f4ad5896c601f47e170743fa
Pietro Lima <pietrovinicius@hotmail.com>
2021-04-23 12:48:53 -0300

    Dado em branco ao abrir o app e remoção dos demais botões

--- app/src/main/java/com/br/diceroller/MainActivity.kt
+++ app/src/main/java/com/br/diceroller/MainActivity.kt
@@ -3,10 +3,15 @@ package com.br.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    /*
+    * A lateinitpalavra-chave promete ao compilador Kotlin que a variável
+    * será inicializada antes que o código chame qualquer operação nela*/
+    lateinit var diceImage : ImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,44 +21,29 @@ class MainActivity : AppCompatActivity() {
         //pegando a referencia do que está no xml da tela
         //será uma constante pois não será alterada
         val btnRollarDado: Button = findViewById(R.id.Btn_rollarDado)
-        val txtResult: TextView = findViewById(R.id.Txt_result)
-        val btnContagem: Button = findViewById(R.id.Btn_contagem)
-        val btnReiniciar: Button = findViewById(R.id.Btn_reiniciar)
+
+        diceImage = findViewById(R.id.Img_result)
 
         //evento de click no button
         btnRollarDado.setOnClickListener{
-            txtResult.text = rolarDado().toString()
+            rolarDado()
         }
-        btnContagem.setOnClickListener{contagem()}
-        btnReiniciar.setOnClickListener{reiniciar()}
     }
 
-    private fun rolarDado(): Int{
+    private fun rolarDado(){
         println("==========Evento de click no botão rolar dados!")
-        val randomInteiro = (1..6).random()
-        println("========== ramdomInteiro: ${randomInteiro}")
-        Toast.makeText(this, "Dado rolado!" , Toast.LENGTH_SHORT).show()
-        return randomInteiro
-    }
+       val randomInteiro = (1..6).random()
+       println("========== ramdomInteiro: ${randomInteiro}")
+       Toast.makeText(this, "Dado rolado!" , Toast.LENGTH_SHORT).show()
 
-    private fun contagem(){
-        println("========== contagem: ")
-        val txtResult: TextView = findViewById(R.id.Txt_result)
-
-        if(txtResult.text == "Olá mundo Kotlin"){
-            txtResult.text = "1"
-        }else{
-            var txtResultInt = txtResult.text.toString().toInt()
-            if(txtResultInt  < 6) {
-                txtResultInt ++
-                txtResult.text = txtResultInt.toString()
-            }
+        val drawableResource = when(randomInteiro){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
-    }
-    private fun reiniciar(){
-        println("========== reiniciar: ")
-        val txtResult: TextView = findViewById(R.id.Txt_result)
-        txtResult.text = "0"
-        println("========== txtResult: ${txtResult.text}")
+        diceImage.setImageResource(drawableResource)
     }
 }
